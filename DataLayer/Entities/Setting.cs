e0de231634cbd3699ec06083a39fe9b942187354
Brian Cullinan <megamindbrian@gmail.com>
2026-03-14 08:23:49 -0700

    sidebar text color fix

--- DataLayer/Entities/Setting.cs
+++ DataLayer/Entities/Setting.cs
@@ -3,13 +3,17 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataLayer.Entities
 {
-    [PrimaryKey(nameof(Name), nameof(Guid), nameof(Role))]
+    [PrimaryKey(nameof(Name), nameof(Guid), nameof(RoleId))]
     [Table("setting")]
     public class Setting : Entity<Setting>
     {
-        public string Name { get; set; }
-        public string Value { get; set; }
-        public string Role { get; set; }
-        public string Guid { get; set; }
+        public string? Name { get; set; }
+        public string? Value { get; set; }
+        public string? RoleId { get; set; }
+        [ForeignKey(nameof(RoleId))]
+        public DataLayer.Entities.Role? Role { get; set; }
+        public string? Guid { get; set; }
+        [ForeignKey(nameof(Guid))]
+        public DataLayer.Entities.User? User { get; set; }
     }
 }
